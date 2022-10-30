04ab457a9e660cb414ae947727de4e77b7eb39a0
Dawid <kantochdawid98@gmail.com>
2022-10-30 10:18:53 +0100

    First concept layout design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { OverlayContainer } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  toggleControl = new FormControl(false);
+  @HostBinding('class') className = '';
+  
   title = 'wlp_view';
+
+  constructor(private overlay: OverlayContainer) { }
+
+  ngOnInit(): void {
+    this.toggleControl.valueChanges.subscribe((darkMode) => {
+      const darkClassName = 'darkMode';
+      this.className = darkMode ? darkClassName : '';
+      if (darkMode) {
+        this.overlay.getContainerElement().classList.add(darkClassName);
+      } else {
+        this.overlay.getContainerElement().classList.remove(darkClassName);
+      }
+    });
+
+    let cc = window as any;
+       cc.cookieconsent.initialise({
+         palette: {
+           popup: {
+             background: "#164969"
+           },
+           button: {
+             background: "#ffe000",
+             text: "#164969"
+           }
+         },
+         theme: "classic",
+         content: {
+            message: "This website uses cookies to ensure you get the best experience on our website.",
+            dismiss: "Dismiss!",
+            link: "Learn more",
+            href:"http://www.google.com"
+         }
+       });
+}
 }
